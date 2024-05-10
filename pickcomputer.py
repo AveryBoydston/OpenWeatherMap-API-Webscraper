a7f110d1d6c575e7ae14170be205fbd4d3217b17
AveryBoydston <avboyds@gmail.com>
2024-05-10 13:42:21 -0500

    Eliminated redundant coding structure
    consolidated the if-elif-else structure into a simple for loop

--- pickcomputer.py
+++ pickcomputer.py
@@ -1,13 +1,14 @@
 import os
 
 def pickcomputer(): #when changing which computer I'm using
-    if os.path.exists("C:/Users/avboy/Documents/GitHub_Personal"): #laptop
-        directory = "C:/Users/avboy/Documents/GitHub_Personal"
+    paths = ["C:/Users/avboy/Documents/GitHub_Personal",
+             "C:/Users/Avery B/Documents/My Documents/GitHub",
+             "G:/Avery's funny business/Github"]
+    for directory in paths:
+        if os.path.exists(directory):
+            return directory
 
-    elif os.path.exists("C:/Users/Avery B/"): #personal desktop pc
-        directory = "C:/Users/Avery B/Documents/My Documents/GitHub"
+    print("Error assigning computer directory in file pickcomputer.py.")
+    exit()
 
-    elif os.path.exists("G:/Avery's funny business/Github/"): #at home pc
-        directory = "G:/Avery's funny business/Github"
-    return directory
 directory = pickcomputer()
